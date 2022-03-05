cb9731081af223df509a72329d8e00401dbfd1a5
j-j-jack <jackosullivan541@gmail.com>
2022-03-05 10:54:02 +0000

    add basic d3 to scroll animation

--- src/components/ScrollAnimation/ScrollAnimation.js
+++ src/components/ScrollAnimation/ScrollAnimation.js
@@ -1,7 +1,12 @@
 import React, { useRef } from 'react';
-import "../css/scroll-animation.css"
+import * as d3 from 'd3';
+
+import "../css/scroll-animation.css";
 
 const ScrollAnimation = () => {
+    const topSvg = useRef();
+    const bottomSvg = useRef();
+
     const gaussian = (min, max) => {
         // https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
         // adapted from Maxwell Collard and Mahdi on Stack Overflow
@@ -24,12 +29,32 @@ const ScrollAnimation = () => {
         w = w + min;
         return w;
     }
-
+    
+    const append = () => {
+        d3.select(topSvg.current).append('circle').attr('cx', '20%' ).attr('cy', '20%').attr('r', 20).style('fill', 'gold').transition().duration(500).remove();
+    }
+    const appendTwo = () => {
+        d3.select(bottomSvg.current).append('circle').attr('cx', '20%' ).attr('cy', '20%').attr('r', 20).style('fill', 'gold').transition().duration(500).remove();
+    }
 
     return (
         <React.Fragment>
-        <div className="top-animation"></div>
-        <div className="bottom-animation"></div>
+        <div className="top-animation">
+            <svg
+                ref={topSvg}
+                className="top-svg"
+                onClick={() => append()}
+            >
+            </svg>    
+        </div>
+        <div className="bottom-animation">
+            <svg
+                ref={bottomSvg}
+                className="bottom-svg"
+                onClick={() => appendTwo()}
+            >
+            </svg>  
+        </div>
         </React.Fragment>
     );
 };
