85eab94101007929c43a9f2cf8888afab8f7851e
j-j-jack <jackosullivan541@gmail.com>
2022-03-06 15:26:49 +0000

    add state to display project title/details

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./css/project-card.css";
 import projectImage from '../assets/images/e-book-club.PNG';
 
 const ProjectCard = () => {
-    return (
-        <div className="project-card-container">
-            <div id="title-outer">
-                <div id="title-inner">
-                    <h1 class="glitch" data-text="Project">Project</h1>
+
+    const [hover, setHover] = useState(false);
+    
+    const renderContent = () => {
+        if (hover) {
+            return (
+                <div className="project-details-container">
+                    <div id="title-outer">
+                        <div id="title-inner">
+                            <h1 class="glitch" data-text="Project">Project</h1>
+                        </div>
+                    </div>
+                </div> 
+            );
+        } else {
+            return (
+                <div className="project-poster-container">
+                    <div id="title-outer">
+                        <div id="title-inner">
+                            <h1 class="glitch" data-text="Project">Project</h1>
+                        </div>
+                    </div>
+                    <div className="project-image-container">
+                        <img className="project-image" src={projectImage}></img>
+                    </div>
+                    <div className="tv-static"></div>
                 </div>
-            </div>
-            <div className="project-image-container">
-                <img className="project-image" src={projectImage}></img>
-            </div>
-            <div className="tv-static"></div>
+                );
+            }
+        
+        }
+    return (
+        <div 
+            className="project-card-container"
+            onMouseOver={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+        >
+            {renderContent()}
         </div>
     );
 };
