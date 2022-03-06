fb38de10e85f6f608832f69472368ec0939c4d39
j-j-jack <jackosullivan541@gmail.com>
2022-03-06 12:54:32 +0000

    add glitch title to project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,10 +5,15 @@ import projectImage from '../assets/images/e-book-club.PNG';
 const ProjectCard = () => {
     return (
         <div className="project-card-container">
-            <h2>Project Title</h2>
+            <div id="title-outer">
+                <div id="title-inner">
+                    <h1 class="glitch" data-text="Project">Project</h1>
+                </div>
+            </div>
             <div className="project-image-container">
                 <img className="project-image" src={projectImage}></img>
             </div>
+            <div className="tv-static"></div>
         </div>
     );
 };
