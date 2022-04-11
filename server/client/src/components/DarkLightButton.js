938ace81e672a8b5e4d0f0636d85b10c6983461d
j-j-jack <jackosullivan541@gmail.com>
2022-04-11 15:01:23 +0100

    rotate sun and moon on click

--- server/client/src/components/DarkLightButton.js
+++ server/client/src/components/DarkLightButton.js
@@ -1,10 +1,21 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import moon from "../assets/images/moon.svg";
 import sun from "../assets/images/sun.svg";
 import "./css/dark-light-button.css";
 
 const DarkLightButton = () => {
     const stylesheetRef = useRef();
+    const sunMoonContainer = useRef();
+    const [lightState, setLightState] = useState('light');
+    
+    const changeRotation = () => {
+        if (lightState === 'light') {
+            setLightState('dark');
+        } else {
+            setLightState('light');
+        }
+    }
+
 
     useEffect(() => {
         const sheets = document.styleSheets;
@@ -16,11 +27,10 @@ const DarkLightButton = () => {
         console.log(stylesheetRef.current);
     }, []);
 
-    return (
-        <div className="dark-light-container-outer">
-            <div className="blocking-div">
-            </div>
-            <div className="sun-moon-container">
+    const renderHelper = () => {
+        if (lightState === "light") {
+            return (
+                <div onClick={() => {changeRotation()}} ref={sunMoonContainer} className="sun-up-container">
             <div> 
                 <img src={sun} />
             </div>
@@ -28,6 +38,26 @@ const DarkLightButton = () => {
                 <img src={moon} />
             </div>
             </div>
+            );
+        } else if (lightState === "dark") {
+            return (
+                <div onClick={() => {changeRotation()}} ref={sunMoonContainer} className="sun-down-container">
+            <div> 
+                <img src={sun} />
+            </div>
+            <div>
+                <img src={moon} />
+            </div>
+            </div>
+            );
+        }
+    }
+
+    return (
+        <div className="dark-light-container-outer">
+            <div className="blocking-div">
+            </div>
+            {renderHelper()}
         </div>
     );
 }
